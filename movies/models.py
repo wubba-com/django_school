f9a5da7d7b5fa428267e704219790a40c165dd59
Sergey <eng.ise8@gmail.com>
2020-10-07 13:06:33 +0300

    fixed a bug in the model Rating on_delete=models.CharFields on on_delete=models.CASCADE

--- movies/models.py
+++ movies/models.py
@@ -124,7 +124,7 @@ class Rating(models.Model):
     """ Рейтинг """
     ip = models.CharField('IP адрес', max_length=15)
     star = models.ForeignKey(RatingStar, on_delete=models.CASCADE, verbose_name='звезда')
-    movie = models.ForeignKey(Movie, on_delete=models.CharField, verbose_name='фильм')
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE, verbose_name='фильм')
 
     def __str__(self):
         return f'{self.star} - {self.movie}'
