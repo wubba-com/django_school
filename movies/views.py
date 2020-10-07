5d5be5c44eafce1b458d91a77ddde47d4034d19e
Sergey <eng.ise8@gmail.com>
2020-10-07 15:33:49 +0300

    create admin and added models, create class Views, first url and first file html

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views.generic.base import View
+from .models import Movie
 
-# Create your views here.
+
+class MoviesView(View):
+    """Список фильмок"""
+
+    def get(self, request): # request - вся инфа присланная от клиента (браузера)
+        movies = Movie.objects.all()
+        return render(request, 'movies/movies_list.html', {'movies_list': movies})
