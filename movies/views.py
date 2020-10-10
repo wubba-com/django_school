5dedf76ece7cd3766a53df1fd2c9a2343de7d1da
Sergey <eng.ise8@gmail.com>
2020-10-10 10:59:12 +0300

    create forms, made reviews

--- movies/views.py
+++ movies/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.base import View
 from django.views.generic import ListView, DetailView
 from .models import Movie
+from .forms import ReviewForm
 
 
 class MoviesView(ListView):
@@ -18,3 +19,19 @@ class MovieDetailView(DetailView):
 
     model = Movie
     slug_field = 'url'
+
+
+class AddReview(View):
+    """Отправка отзыва"""
+
+    def post(self, request, pk):
+        form = ReviewForm(request.POST)  # Импортировали форму и создали объект формы.
+        # Затем используя форму и передавая в нее request.POST таким образом
+        # джанго заполнит нашу форму данными, которые пришли к нам из запроса
+        movie = Movie.objects.get(id=pk)
+        if form.is_valid():
+            form = form.save(commit=False)  # commit=False, значит что мы хотим приостановить
+            # сохранение нашей формы. Теперь мы можем внести изменеие в нашу форму
+            form.movie = movie
+            form.save()
+        return redirect(movie.get_absolute_url())
